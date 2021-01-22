4f9c398468a598af8ab24411c9ff1600eec6667f
vpoat <vpoat@pop-os.localdomain>
2021-01-22 08:25:57 +0700

    update

--- img_2_hist.py
+++ img_2_hist.py
@@ -2,9 +2,29 @@ import numpy as np
 import cv2
 
 
-BIN = 8
+M = 8  # nums of bin
 L = 256
 
-img = cv2.imread('./lena.png', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('', img)
+DELTA = (L - 1) / M
+
+
+def get_pixel_bin(v):
+    for i in range(M):
+        lower_d = DELTA * i
+        if i != M:
+            upper_d = DELTA * (i + 1)
+        else:
+            upper_d = DELTA * (i + 1) + 1
+        if lower_d <= v and v < upper_d:
+            return i
+
+    return -1
+
+def get_hist(img):
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            get_pixel_bin(img[i, j])
+
+img = cv2.imread("./lena.png", cv2.IMREAD_GRAYSCALE)
+cv2.imshow("", img)
 cv2.waitKey(0)
